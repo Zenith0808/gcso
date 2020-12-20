391af6527af4cb3c894bed701e405dfd203e5132
Zenith0808 <75840283+Zenith0808@users.noreply.github.com>
2020-12-20 13:17:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,43 @@
-var frect,mrect;
-
-
-
+var car,wall;
+var speed,weight;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1600,400);
+car = createSprite(200,200,50,50);
+wall = createSprite(1200,200,60,100);
 
-frect=createSprite(300,300,50,20);
-mrect=createSprite(400, 100, 70, 50);
-mrect.shapeColor="blue";
-frect.shapeColor="orange";
+  car.velocityX = 5;
+
+ speed=random(25,31);
+ weight=random(400,1500);
+
 }
 
 function draw() {
-  background(255,255,255);  
-mrect.x=World.mouseX
-mrect.y=World.mouseY
+  background("black");
+  
 
-if(mrect.y-frect.y<mrect.height/2+frect.height/2
-&&  frect.y-mrect.y<mrect.height/2+frect.height/2)
-{
-  mrect.shapeColor="orange";
-  frect.shapeColor="blue";
-
-}
-  else{
-    mrect.shapeColor="blue";
-    frect.shapeColor="orange";
-
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor=color("yellow");
+      wall.shapeColor=color("white");
+    }
+    if(deformation<180 && deformation>100)
+    {
+      car.shapeColor=color("green");
+      wall.shapeColor=color("white");
+    }
+    if(deformation<100)
+    {
+      car.shapeColor=color("red");
+      wall.shapeColor=color("white");
+    }
   }
+
   drawSprites();
+  
 }
